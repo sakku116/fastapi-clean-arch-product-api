f713b1e3fd69fa2979b9107409074be1f7ae4285
zakky <zakky1164@gmail.com>
2023-12-11 14:51:09 +0700

    delete _coll_name from base model

--- domain/model/base_model.py
+++ domain/model/base_model.py
@@ -3,8 +3,6 @@ from bson import Int64
 
 
 class MyBaseModel(BaseModel):
-    _coll_name: str = ""
-
     _id: str = ""
     created_at: Int64 = 0
     updated_at: Int64 = 0
--- domain/model/user.py
+++ domain/model/user.py
@@ -3,7 +3,5 @@ from .base_model import MyBaseModel
 COLL = "users"
 
 class UserModel(MyBaseModel):
-    _coll_name: str = "users"
-
     username: str = ""
     password: str = ""
