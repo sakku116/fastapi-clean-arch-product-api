5dc3f8438bfe3c2227fdba933cc029e8ba9eaf18
zakky <zakky1164@gmail.com>
2023-12-12 10:45:21 +0700

    move exception package to core

--- exception/http.py
+++
@@ -1,8 +0,0 @@
-class CustomHTTPExc(Exception):
-    def __init__(self, status_code=500, message="exception", data=None, detail=""):
-        self.status_code = status_code
-        self.message = message
-        self.data = data
-        self.detail = detail
-
-        super().__init__(message if not detail else detail)
