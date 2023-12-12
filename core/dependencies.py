19a8b706fbae0c01b56bdf4142437a1a425c7db5
zakky <zakky1164@gmail.com>
2023-12-12 14:22:55 +0700

    remove Bearer prefix validation cuz it already removed by fastapi

--- core/dependencies.py
+++ core/dependencies.py
@@ -13,12 +13,5 @@ reusable_token = OAuth2PasswordBearer("/auth/login")
 async def verifyTokenDependency(
     auth_service: AuthService = Depends(), token=Depends(reusable_token)
 ):
-    if "Bearer " not in token:
-        exc = CustomHTTPExc(
-            status_code=401,
-            message="Invalid token",
-        )
-        logger.error(exc)
-        raise exc
     current_user = auth_service.checkToken(token=token)
     return current_user
