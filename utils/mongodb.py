4ab90519e3a107a201abf9051b775d527f424c5d
zakky <kycaft1164@gmail.com>
2023-12-13 22:37:50 +0700

    feat: dynamically import model file and ensure it indexes

--- utils/mongodb.py
+++ utils/mongodb.py
@@ -3,6 +3,10 @@ import logging
 from pymongo.database import Database
 
 from domain.model import user
+import os
+import importlib
+import inspect
+from domain.model.base_model import MyBaseModel
 
 logger = logging.getLogger(__name__)
 
@@ -10,9 +14,21 @@ logger = logging.getLogger(__name__)
 def ensureIndexes(db: Database):
     logger.info("Ensuring mongodb collection indexes")
 
-    models = [user.UserModel()]
-    for model in models:
-        logger.info(f"\tEnsuring index for '{model._coll_name}' collection")
-        for index in model._indexes:
-            logger.info(f"\t\tindex: {index.model_dump()}")
-            db[model._coll_name].create_index(**index.model_dump())
+    model_filenames = os.listdir("domain/model")
+    for filename in model_filenames:
+        if filename.endswith(".py") and filename.removesuffix(".py") not in [
+            "__init__",
+            "base_model",
+        ]:
+            module_path = __import__(
+                f"domain.model.{filename[:-3]}", fromlist=[filename[:-3]]
+            )
+            for member_name, member in inspect.getmembers(module_path):
+                if inspect.isclass(member) and member_name.lower().endswith("model"):
+                    member: MyBaseModel = member()
+                    coll_name = member._coll_name
+                    indexes = member._indexes
+                    logger.info(f"\tEnsuring index for '{coll_name}' collection")
+                    for index in indexes:
+                        logger.info(f"\t\tindex: {index.model_dump()}")
+                        db[coll_name].create_index(**index.model_dump())
