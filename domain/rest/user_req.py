794909fc53d3eff81722bee5f7ff227fd1c8a80d
zakky <zakky1164@gmail.com>
2023-12-13 11:01:03 +0700

    feat: PostCreateUserReq schema

--- domain/rest/user_req.py
+++ domain/rest/user_req.py
@@ -1,7 +1,13 @@
 from .common import CommonReq
 from typing import Literal
+from pydantic import BaseModel
 
 class GetUsersReq(CommonReq):
     role: Literal["all", "user", "admin"] = "all"
     username: str = ""
     sort_by: Literal["created_at", "username"] = "created_at"
+
+class PostCreateUserReq(BaseModel):
+    username: str
+    password: str
+    role: Literal["admin", "user"]
