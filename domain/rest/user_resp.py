fabc88cbd8d4936ab1cbaaa71156129fc9a73ef5
zakky <zakky1164@gmail.com>
2023-12-13 11:01:52 +0700

    feat: PostCreateUserResp payload and schema inheritance

--- domain/rest/user_resp.py
+++ domain/rest/user_resp.py
@@ -2,14 +2,21 @@ from pydantic import BaseModel
 from bson import Int64
 from .base import BaseModelSchema
 
-class GetUsersResp(BaseModelSchema):
+
+class BaseUserResp(BaseModel):
     username: str = ""
     role: str = ""
     is_active: bool = True
     last_login: int = 0
 
-class GetUserByIdResp(BaseModelSchema):
-    username: str = ""
-    role: str = ""
-    is_active: bool = True
-    last_login: int = 0
+
+class GetUsersResp(BaseUserResp, BaseModelSchema):
+    pass
+
+
+class GetUserByIdResp(BaseUserResp, BaseModelSchema):
+    pass
+
+
+class PostCreateUserResp(BaseUserResp, BaseModelSchema):
+    pass
