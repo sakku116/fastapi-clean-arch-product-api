8153c67211bd19a05573606338623b232d48de09
zakky <zakky1164@gmail.com>
2023-12-13 10:58:59 +0700

    add created_by and updated_by field

--- domain/model/base_model.py
+++ domain/model/base_model.py
@@ -1,6 +1,7 @@
 from pydantic import BaseModel
 from bson import Int64
 
+
 class _MyBaseModel_Index(BaseModel):
     """
     this attributes is same as pymongo.collection.Collection.create_index() args
@@ -9,18 +10,18 @@ class _MyBaseModel_Index(BaseModel):
     keys: list = []
     unique: bool = False
 
+
 class MyBaseModel(BaseModel):
     _coll_name: str = ""
     _indexes: list[_MyBaseModel_Index] = [
-        _MyBaseModel_Index(
-            keys=[("id", 1)],
-            unique=True
-        )
+        _MyBaseModel_Index(keys=[("id", 1)], unique=True)
     ]
 
     id: str = ""
     created_at: Int64 = Int64(0)
     updated_at: Int64 = Int64(0)
+    created_by: str = ""
+    updated_by: str = ""
 
     class Config:
         arbitrary_types_allowed = True
--- domain/rest/base.py
+++ domain/rest/base.py
@@ -5,5 +5,7 @@ class BaseModelSchema(BaseModel):
     id: str = ""
     created_at: int = 0
     updated_at: int = 0
+    created_by: str = ""
+    updated_by: str = ""
     class Config:
         arbitrary_types_allowed = True
